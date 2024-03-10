0a82e9d7c4f92909864fa1ff9cd71b409809f70e
Dom-iha <taqib.ibrahim@gmail.com>
2024-03-10 11:19:54 +0100

    updated svg style

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -9,12 +9,12 @@ export const Copy = () => (
     <path
       d='M6 11C6 8.17157 6 6.75736 6.87868 5.87868C7.75736 5 9.17157 5 12 5H15C17.8284 5 19.2426 5 20.1213 5.87868C21 6.75736 21 8.17157 21 11V16C21 18.8284 21 20.2426 20.1213 21.1213C19.2426 22 17.8284 22 15 22H12C9.17157 22 7.75736 22 6.87868 21.1213C6 20.2426 6 18.8284 6 16V11Z'
       stroke='currentColor'
-      stroke-width='1.5'
+      strokeWidth='1.5'
     />
     <path
       d='M6 19C4.34315 19 3 17.6569 3 16V10C3 6.22876 3 4.34315 4.17157 3.17157C5.34315 2 7.22876 2 11 2H15C16.6569 2 18 3.34315 18 5'
-      stroke='#1C274C'
-      stroke-width='1.5'
+      stroke='currentColor'
+      strokeWidth='1.5'
     />
   </svg>
 );
@@ -27,23 +27,23 @@ export const Home = () => (
     fill='none'
     xmlns='http://www.w3.org/2000/svg'
   >
-    <g id='SVGRepo_bgCarrier' stroke-width='0'></g>
+    <g id='SVGRepo_bgCarrier' strokeWidth='0'></g>
     <g
       id='SVGRepo_tracerCarrier'
-      stroke-linecap='round'
-      stroke-linejoin='round'
+      strokeLinecap='round'
+      strokeLinejoin='round'
     ></g>
     <g id='SVGRepo_iconCarrier'>
       <path
         d='M2 12.2039C2 9.91549 2 8.77128 2.5192 7.82274C3.0384 6.87421 3.98695 6.28551 5.88403 5.10813L7.88403 3.86687C9.88939 2.62229 10.8921 2 12 2C13.1079 2 14.1106 2.62229 16.116 3.86687L18.116 5.10812C20.0131 6.28551 20.9616 6.87421 21.4808 7.82274C22 8.77128 22 9.91549 22 12.2039V13.725C22 17.6258 22 19.5763 20.8284 20.7881C19.6569 22 17.7712 22 14 22H10C6.22876 22 4.34315 22 3.17157 20.7881C2 19.5763 2 17.6258 2 13.725V12.2039Z'
         stroke='currentColor'
-        stroke-width='1.5'
+        strokeWidth='1.5'
       ></path>{' '}
       <path
         d='M15 18H9'
         stroke='currentColor'
-        stroke-width='1.5'
-        stroke-linecap='round'
+        strokeWidth='1.5'
+        strokeLinecap='round'
       ></path>{' '}
     </g>
   </svg>
